52e11e807d425360d76c043690d9bc63831e0413
cltownes <cltownes@outlook.com>
2022-04-08 09:24:05 -0400

    Add project files.

---
+++ Tracking.Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Tracking.Client;
+
+
+
+HttpClient client = new();
+client.BaseAddress = new Uri("https://localhost:7165");
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+// Retreive all of the issues and display in console app
+HttpResponseMessage response = await client.GetAsync("api/issue");
+response.EnsureSuccessStatusCode();
+
+if (response.IsSuccessStatusCode)
+{
+    var issues = await response.Content.ReadFromJsonAsync<IEnumerable<IssueDto>>();
+
+    foreach (var issue in issues)
+    {
+        Console.Write(issue.Title + "\n");
+    }
+}
+else
+{
+    Console.WriteLine("No results");
+}
+Console.ReadLine();
+
+
---
+++ TrackingApi/Migrations/20220406195640_AddIdRequire.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrackingApi.Migrations
+{
+    public partial class AddIdRequire : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
